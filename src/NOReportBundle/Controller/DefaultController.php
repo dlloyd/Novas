4f17d0a9679f36cab1edfb664ce3f6bfec4457e1
Lloyd Dingalt <ldingalt@gmail.com>
2017-10-18 00:26:50 +0200

    L'administrateur peut maintenant voir les rapports des entreprises

--- src/NOReportBundle/Controller/DefaultController.php
+++ src/NOReportBundle/Controller/DefaultController.php
@@ -64,7 +64,7 @@ class DefaultController extends Controller
                 } 
                 
                if($lines[1]!=0){  // la condition évite la division par zéro
-               $average[$subId->getName()] = (int)($totalAnswersValue/(int)$lines[1]);
+               $average[$subId->getFamily()->getName().':'.$subId->getName()] = (int)($totalAnswersValue/(int)$lines[1]);
                }
             }
             if($data['chart'] == 'radar'){
@@ -78,6 +78,75 @@ class DefaultController extends Controller
 
         return $this->render('NOReportBundle:Default:index.html.twig',array('form' => $form->createView(),));
 
+   }
+
+
+   public function adminCompanyReportAction($companyId,Request $request){
+      $data = array();
+      $companySubFams = array();
+      $em = $this->getDoctrine()->getManager();
+      $company = $em->getRepository('CompanyBundle:Company')->find($companyId);
+      
+      foreach ($company->getCompanyQuestionSubFamAccess() as  $subAccess) {
+         array_push($companySubFams, $subAccess->getQuestionSubFamily());
+      }
+
+
+      $form = $this->createFormBuilder($data)
+            ->add('subFams',EntityType::class,array(
+                    'class'    => 'NODiagBundle:QuestionSubFamily',
+                    'choices'  => $companySubFams,
+                    'choice_label' => function ($subFamily) {
+                            return $subFamily->getName();
+                        },
+                    'group_by' => function($subFamily) {
+                                        return $subFamily->getFamily()->getName();
+                                    },
+                    'required' => false, 
+                    'expanded' => true,
+                    'multiple' => true ,))
+            ->add('chart',ChoiceType::class,array(
+                     'choices'  => array(
+                                'radar' => 'radar',
+                                'bar' => 'bar',
+                            ),
+                     'required' => true, 
+                    'expanded' => true,
+                    'multiple' => false ,))
+                ->getForm();
+
+      $form->handleRequest($request);
+
+      if($form->isSubmitted() && $form->isValid()){
+         $average = array();
+         $data= $form->getData();
+            
+            foreach ($data['subFams'] as $subId) {
+               $responses = $em->getRepository('NODiagBundle:ResponseQuestionCompany')
+                                 ->findTotalSubFamilyAnsweredResponses($company->getId(),$subId); 
+               $lines = $em->getRepository('NODiagBundle:ResponseQuestionCompany')
+                                       ->findTotalSubFamilyAnsweredCount($company->getId(),$subId);
+                                       
+               $totalAnswersValue = 0;
+               foreach ($responses as $resp) {
+                  $totalAnswersValue += $resp->getScoring();
+                  
+                } 
+                
+               if($lines[1]!=0){  // la condition évite la division par zéro
+               $average[$subId->getFamily()->getName().':'.$subId->getName()] = (int)($totalAnswersValue/(int)$lines[1]);
+               }
+            }
+            if($data['chart'] == 'radar'){
+               return $this->render('NOReportBundle:Default:radar.html.twig',array('average'=>$average,));
+            }
+            else{
+               return $this->render('NOReportBundle:Default:bar.html.twig',array('average'=>$average,));
+            }
+            
+        }
+
+        return $this->render('NOReportBundle:Default:index.html.twig',array('form' => $form->createView(),'company'=> $company ));
    }
 
 
